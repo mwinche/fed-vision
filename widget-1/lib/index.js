98869f7c76e8115fdf356bca4c77dd45e58b8ed5
Matt Winchester <matthewwinchester@workfront.com>
2017-04-17 15:31:50 -0600

    commit progress + todos

--- widget-1/lib/index.js
+++ widget-1/lib/index.js
@@ -1,3 +1,8 @@
 import Toggle from './Toggle';
 
 export default Toggle;
+
+//TODO prettier for uniform code styles
+//TODO husky prepush/precommit test+lint
+//TODO testing in react storybook
+//TODO changelog generation
