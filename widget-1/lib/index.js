7e386ade15360c1cb3830a509623f486c8b2931e
Matt Winchester <matthewwinchester@workfront.com>
2017-05-03 21:47:32 -0600

    chore: updated TODO

--- widget-1/lib/index.js
+++ widget-1/lib/index.js
@@ -4,6 +4,6 @@ export default Toggle;
 
 //TODO prettier for uniform code styles
 //TODO husky prepush/precommit test+lint
-//TODO testing in react storybook
+//TODO testing in react storybook (not sure if this is doable, without writing a plugin)
 //TODO changelog generation
 //TODO customize eslint (airbnb is too restrictive)
