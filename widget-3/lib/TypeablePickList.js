ced7ee0e711c40e55d2836beb4c0b45dbcabde8f
Matt Winchester <matthewwinchester@workfront.com>
2017-06-14 14:00:50 -0600

    refactor: let the component manage the selected state

--- widget-3/lib/TypeablePickList.js
+++ widget-3/lib/TypeablePickList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { css } from 'glamor';
 import PropTypes from 'prop-types';
 
@@ -9,41 +9,73 @@ const selectedClass = {
 
 const isSelected = (item, selected) => selected === item;
 
-const TypeablePickList = ({items, onNewItem, onSelect, selected}) => {
-  let input;
-  
-  return (<div>
-    <input type="text"
-      ref={_input => input = _input}
-      onKeyPress={({key}) => {
-        if(key === 'Enter'){
-          onNewItem(input.value);
-          input.value = '';
+class TypeablePickList extends Component {
+  static defaultProps = {
+    items: [],
+    onNewItem: () => {},
+    onSelect: () => {},
+  }
+
+  static propTypes = {
+    items: PropTypes.array,
+    onNewItem: PropTypes.func,
+    onSelect: PropTypes.func,
+  }
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      selected: props.selected
+    };
+  }
+
+  componentWillReceiveProps(props){
+    const { selected } = props;
+
+    if(this.state.selected !== selected){
+      this.setState({ selected });
+    }
+  }
+
+  keyPress({key}){
+    const { onNewItem } = this.props;
+
+    if(key === 'Enter'){
+      onNewItem(this.input.value);
+      this.input.value = '';
+    }
+  }
+
+  selectItem(selected){
+    const { onSelect } = this.props;
+
+    this.setState(
+      { selected },
+      () => onSelect(selected)
+    );
+  }
+
+  render(){
+    const { items } = this.props;
+    const { selected } = this.state;
+
+    return (<div>
+      <input type="text"
+        ref={input => this.input = input}
+        onKeyPress={this.keyPress.bind(this)}/>
+      <ul>
+        {
+          items.map(item => <li
+            key={`item-${item}`}
+            onClick={() => this.selectItem(item)}
+            className={isSelected(item, selected) && css(selectedClass)}>
+            {item}
+          </li>)
         }
-      }}/>
-    <ul>
-      {
-        items.map(item => <li
-          key={`item-${item}`}
-          onClick={() => onSelect(item)}
-          className={isSelected(item, selected) && css(selectedClass)}>
-          {item}
-        </li>)
-      }
-    </ul>
-  </div>);
-};
-
-TypeablePickList.defaultProps = {
-  items: [],
-  onNewItem: () => {},
-  onSelect: () => {},
-};
-
-TypeablePickList.propTypes = {
-  items: PropTypes.array,
-  onNewItem: PropTypes.func,
-  onSelect: PropTypes.func,
-};
+      </ul>
+    </div>);
+  }
+}
 
 export default TypeablePickList;
--- widget-3/lib/TypeablePickList.story.js
+++ widget-3/lib/TypeablePickList.story.js
@@ -8,50 +8,32 @@ class StateWrapper extends Component {
   constructor(props){
     super(props);
 
-    this.state = { items: props.items, selected: props.selected };
+    const { selected, items } = props;
+    this.state = { selected, items };
+  }
+
+  newItem(item){
+    const transformedItem = item.trim().replace(/\s+/g, '-');
+
+    this.setState(({ items }) => {
+      return {
+        selected: item,
+        items: uniqueAdd(items, transformedItem)
+      };
+    });
   }
 
   render(){
     const { items, selected } = this.state;
 
-    const newItem = (item => {
-      const transformedItem = item.trim().replace(/\s+/g, '-');
-
-      this.setState(({ items }) => {
-        return {
-          selected: transformedItem,
-          items: uniqueAdd(items, transformedItem)
-        };
-      })
-    }).bind(this);
-
     return <TypeablePickList
       items={items}
       selected={selected}
-      onSelect={item => this.setState({ selected: item })}
-      onNewItem={newItem} />;
+      onNewItem={this.newItem.bind(this)} />;
   }
 }
 
 storiesOf('TypeablePickList', module)
   .add('default', () => {
     return <StateWrapper items={['one', 'two']} selected="one" />;
-  })
-  // .add('initially on', () => (
-  //   <Toggle on />
-  // ))
-  // .add('disabled (off)', () => (
-  //   <Toggle disabled />
-  // ))
-  // .add('disabled (on)', () => (
-  //   <Toggle disabled on />
-  // ))
-  // .add('override active color', () => (
-  //   <Toggle on activeColor="green" />
-  // ))
-  // .add('override inactive color', () => (
-  //   <Toggle inactiveColor="red" />
-  // ))
-  // .add('override handle color', () => (
-  //   <Toggle handleColor="gray" />
-  // ));
+  });
