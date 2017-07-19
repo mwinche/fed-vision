8806c3e3233f9e85baa6d36c9c0e638183e2090d
Matt Winchester <matthewwinchester@workfront.com>
2017-07-19 14:42:45 -0600

    chore: adding prod bundle

--- service-1/webpack.config.js
+++ service-1/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+const PROD = process.env.NODE_ENV === 'production';
+
+const baseConfig = {
   entry: './bundle.js',
   output: {
     path: path.resolve('./dist'),
@@ -22,3 +24,16 @@ module.exports = {
     })
   ]
 };
+
+const prodConfig = Object.assign({}, baseConfig, {
+  resolve: {
+    alias: {
+      'react': require.resolve('react/dist/react.min'),
+      'react-dom': require.resolve('react-dom/dist/react-dom.min'),
+      'react-redux': require.resolve('react-redux/dist/react-redux.min'),
+      'glamor': require.resolve('glamor/es6')
+    }
+  }
+});
+
+module.exports = PROD ? prodConfig : baseConfig;
