7a39db29cb6c77f626c291bd3207f35f5f6a993a
Matt Winchester <matthewwinchester@workfront.com>
2017-07-26 13:48:06 -0600

    do the redirect on lambda

--- service-1/index.js
+++ service-1/index.js
@@ -2,8 +2,22 @@ const entry = require('./webpack-assets.json').main.js;
 
 const static = require('./dist/server').default([ entry ]);
 
+const ENDING_SLASH = /\/$/;
+
+const redirect = (Location) => ({
+  body: '',
+  statusCode: 301,
+  headers: { Location }
+});
+
 const main = ({ requestContext }, context, cb) => {
-  console.log(requestContext);
+  const { path } = requestContext;
+  
+  if(path.match(ENDING_SLASH) === null){
+    context.succeed(redirect(`${path}/`));
+
+    return;
+  }
 
   const body = static();
   const statusCode = 200;
